ec3a5f766768072a2f61b95741ccdd7e03e65842
Antoine Tanzilli <antoine@tanzil.li>
2020-03-05 22:20:22 +0100

    Initial commit

---
+++ notify.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports.handler = async (event) => {
+    console.log('Received event', event);
+
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({ message: `success - ${Date.now()}` })
+    };
+
+    return response;
+};
